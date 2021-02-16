9993757de6a4180990740ca624516cc433a9087c
slesolliec <slesolliec@gmail.com>
2021-02-16 14:57:33 +0100

    add footer to git

---
+++ docs/.vuepress/components/Footer.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <footer>
+
+		how2Stellar.ooo helped you? Make us happy by sending any amount of Lumens to<br>
+		<a href="web+stellar:pay?destination=GD24ZNCKDYYKWDCHN77W6IZMYRCTBODLSJBQ4WJBIXVPYFJB66IGHOW2">GD24ZNCKDYYKWDCHN77W6IZMYRCTBODLSJBQ4WJBIXVPYFJB66IGHOW2</a>
+
+  </footer>
+</template>
+
+<style lang="stylus" scoped>
+footer
+	background #339933
+	color white
+	margin 2em 0
+	text-align center
+	padding 0.5em 2em
+	box-shadow 1px 1px 10px silver
+	opacity 0.6
+
+footer a
+	color white
+	font-family 'Courier New'
+	text-decoration none
+	font-weight bold
+
+</style>
+
+
